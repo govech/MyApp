e1e2536911cdd55b22a4401813adcdc75d2a1f0b
zhangsan <1299940308@qq.com>
2025-05-28 16:45:53 +0800

    启用ViewBinding

--- base/src/main/java/com/example/base/SettingsActivity.kt
+++ base/src/main/java/com/example/base/SettingsActivity.kt
@@ -1,25 +1,30 @@
 package com.example.base
 
 import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.base.databinding.ActivitySettingsBinding
 import com.example.constant.RouterPath
 import com.example.utils.NightModeUtils
+import com.example.utils.ktx.binding
+
 @Route(path = RouterPath.Setting.BASE_SETTING)
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity() {
+
+    private val binding by binding(ActivitySettingsBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
 
-        findViewById<Button>(R.id.btnDay).setOnClickListener {
+        binding.btnDay.setOnClickListener{
             NightModeUtils.setNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
-        findViewById<Button>(R.id.btnNight).setOnClickListener {
+        
+        binding.btnNight.setOnClickListener{
             NightModeUtils.setNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
-        findViewById<Button>(R.id.btnSystem).setOnClickListener {
+        
+        binding.btnSystem.setOnClickListener{
             NightModeUtils.setNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         }
     }
--- feature_login/src/main/java/com/example/feature_login/LoginActivity.kt
+++ feature_login/src/main/java/com/example/feature_login/LoginActivity.kt
@@ -1,23 +1,23 @@
 package com.example.feature_login
 
 import android.os.Bundle
-import android.widget.Button
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.example.base.BaseActivity
 import com.example.constant.RouterPath
+import com.example.feature_login.databinding.ActivityLoginBinding
+import com.example.utils.ktx.binding
 
 @Route(path = RouterPath.Login.LOGIN_ACTIVITY)
 class LoginActivity : BaseActivity() {
+    private val mBinding by binding(ActivityLoginBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        btnLogin.setOnClickListener {
+
+        mBinding.btnLogin.setOnClickListener {
             // 这里可以添加登录逻辑
         }
-        val btnSettings = findViewById<Button>(R.id.btn_settings)
-        btnSettings.setOnClickListener {
+        mBinding.btnSettings.setOnClickListener {
             ARouter.getInstance().build(RouterPath.Setting.BASE_SETTING).navigation()
         }
     }
